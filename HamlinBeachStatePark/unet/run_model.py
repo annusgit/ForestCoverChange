c52fb2225572de1858916d763a3f35c2520390ce
annusgit <annusgit@gmail.com>
2018-07-12 11:58:35 +0200

    some cleanup

--- HamlinBeachStatePark/unet/run_model.py
+++ HamlinBeachStatePark/unet/run_model.py
@@ -41,27 +41,25 @@ if __name__ == '__main__':
                 label = val_label[patch*i:patch*i+patch, j*patch:j*patch+patch].astype('int32')
                 seg_map = (val_data[patch*i:patch*i+patch,
                            j*patch:j*patch+patch, 6]*1/65535).astype(np.int32)
-                print('w = {}-{}, h = {}-{}'.format(patch*i, patch*i+patch, j*patch, j*patch+patch))
+                print('log: w = {}-{}, h = {}-{}'.format(patch*i, patch*i+patch, j*patch, j*patch+patch), end='')
                 test_x = torch.FloatTensor(image.transpose(2,1,0)).unsqueeze(0).cuda()
                 out_x, pred = net(test_x)
                 pred = (pred.squeeze(0).cpu().numpy().transpose(1,0)+1) * seg_map
                 accuracy = (pred == label).sum() * 100 /(256**2)
-                print('accuracy = {:.5f}%'.format(accuracy))
+                print(': accuracy = {:.5f}%'.format(accuracy))
                 net_accuracy.append(accuracy)
-
-        #get vegetation count
-        unique, counts = np.unique(pred, return_counts=True)
-        dict_c = dict(zip(unique, counts))
-        #print(dict_c)
-        if 0 in dict_c.keys():
-            zero_count += dict_c[0]
-        if 2 in dict_c.keys():
-            veg_count += dict_c[2]
-        if 13 in dict_c.keys():
-            veg_count += dict_c[13]
-        if 14 in dict_c.keys():
-            veg_count += dict_c[14]
-        print()
+                #get vegetation count
+                unique, counts = np.unique(pred, return_counts=True)
+                dict_c = dict(zip(unique, counts))
+                #print(dict_c)
+                if 0 in dict_c.keys():
+                    zero_count += dict_c[0]
+                if 2 in dict_c.keys():
+                    veg_count += dict_c[2]
+                if 13 in dict_c.keys():
+                    veg_count += dict_c[13]
+                if 14 in dict_c.keys():
+                    veg_count += dict_c[14]
 
         mean_accuracy = np.asarray(net_accuracy).mean()
         print('total accuracy = {:.5f}%'.format(mean_accuracy))
