4ab9dc63af458c15b85ca185aa220887df41caff
annusgit <annusgit@gmail.com>
2018-07-14 19:06:30 +0200

    working wrong on gpu

--- HamlinBeachStatePark/unet/run_model.py
+++ HamlinBeachStatePark/unet/run_model.py
@@ -1,7 +1,7 @@
 
 
 """
-    Model for Unet (pretrained in Matlab)
+    Use Unet model (pretrained in Matlab) and evaluate it
 """
 
 from __future__ import print_function
@@ -30,25 +30,29 @@ if __name__ == '__main__':
     # divide it into patches
     print('after padding: val_data shape = {}, val_label shape = {}'.format(val_data.shape, val_label.shape))
     val_data = np.reshape(val_data, newshape=(-1, 256, 256, 7))
+    seg_map_full = (1/65535*val_data[:,:,:,6]).astype(np.int32); val_data = val_data[:,:,:,:6]
     val_label = np.reshape(val_label, newshape=(-1, 256, 256))
     print('after reshaping: val_data shape = {}, val_label shape = {}'.format(val_data.shape, val_label.shape))
     net_accuracy = []
-    mean = np.load(os.path.join(dir_path, 'mean.npy'))
+    # take everything onto the gpu
+    mean = torch.FloatTensor(np.load(os.path.join(dir_path, 'mean.npy')).astype(np.float32)).cuda()
     zero_count = 0
     veg_count = 0
+    val_data_gpu = torch.FloatTensor(val_data.astype(np.float32)).cuda()
+    # val_label = torch.Tensor(val_label).long()
+    print('final array on gpu = ', val_data_gpu.size())
     if True:
-        for i in range(val_data.shape[0]//patch):
-            for j in range(val_data.shape[1]//patch):
-                image = val_data[patch*i:patch*i+patch, j*patch:j*patch+patch, :6]
+        num_batches = val_data.shape[0]//batch_size
+        for i in range(num_batches): ## val_data.shape[0] is the number of available images
+                image = val_data_gpu[batch_size*i:batch_size*i+batch_size, :, :, :] # get a batch of "batch_size" images
+                label = val_label[batch_size*i:batch_size*i+batch_size, :, :]
+                seg_map = seg_map_full[batch_size*i:batch_size*i+batch_size, :, :]
                 # normalize using pretrained mean
                 image = image - mean
-                label = val_label[patch*i:patch*i+patch, j*patch:j*patch+patch].astype('int32')
-                seg_map = (val_data[patch*i:patch*i+patch,
-                           j*patch:j*patch+patch, 6]*1/65535).astype(np.int32)
-                print('log: w = {}-{}, h = {}-{}'.format(patch*i, patch*i+patch, j*patch, j*patch+patch), end='')
-                test_x = torch.FloatTensor(image.transpose(2,1,0)).unsqueeze(0).cuda()
+                # reshape as (batches, channels, w, h)
+                test_x = image.permute(0,3,2,1)
                 out_x, pred = net(test_x)
-                pred = (pred.squeeze(0).cpu().numpy().transpose(1,0)+1) * seg_map
+                pred = (pred.cpu().numpy().transpose(0,2,1)+1) * seg_map
                 #get vegetation count
                 unique, counts = np.unique(pred, return_counts=True)
                 dict_c = dict(zip(unique, counts))
@@ -58,8 +62,8 @@ if __name__ == '__main__':
                     veg_count += dict_c[x]
                 zero_count += dict_c[0] if 0 in dict_c.keys() else 0
                 # get accuracy metric
-                accuracy = (pred == label).sum() * 100 / 256**2
-                print(': accuracy = {:.5f}%'.format(accuracy))
+                accuracy = (pred == label).sum() * 100 / (batch_size*256**2)
+                print('batch {}/{}: accuracy = {:.5f}%'.format(i+1, num_batches, accuracy))
                 net_accuracy.append(accuracy)
         mean_accuracy = np.asarray(net_accuracy).mean()
         print('total accuracy = {:.5f}%'.format(mean_accuracy))
