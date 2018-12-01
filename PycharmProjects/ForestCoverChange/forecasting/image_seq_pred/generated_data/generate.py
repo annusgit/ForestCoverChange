f81e5dc6cc27db83a30efde55c333338c55e078d
AnnusZulfiqar <annusgit@gmail.com>
2018-12-01 17:02:58 +0500

    problematic pakistani test added

--- PycharmProjects/ForestCoverChange/forecasting/image_seq_pred/generated_data/generate.py
+++ PycharmProjects/ForestCoverChange/forecasting/image_seq_pred/generated_data/generate.py
@@ -1,36 +1,53 @@
 
 
-from __future__ import print_function, division
+from __future__ import print_function
+from __future__ import division
 import time
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as pl
+# random.seed(int(time.time()))
 
 
-def get_neighbour(img, pixel, shape):
-    x_max, y_max = shape
+def get_neighbour(img, pixel, pix_val=0):
+    x_max, y_max = img.shape
     i, j = pixel
     possible_neighbours = [(i - 1, j - 1), (i, j - 1), (i + 1, j - 1),
                            (i - 1, j)                , (i + 1, j),
                            (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)]
     # those that are within legitimate boundaries of the image
-    legitimate_neighbours = [(x,y) for (x,y) in possible_neighbours
-                             if x >= 0 and x < x_max and y >= 0 and y < y_max]
-    growing_neighbours = [(x,y) for (x,y) in legitimate_neighbours if img[x,y] == 0]
-    print(len(growing_neighbours))
-    if len == 0:
-        return np.random.choice(legitimate_neighbours)
-    return np.random.choice(growing_neighbours)
+    legitimate_neighbours = [(x,y) for (x,y) in possible_neighbours if 0 <= x < x_max and 0 <= y < y_max]
+    # growing_neighbours = [(x,y) for (x,y) in legitimate_neighbours if img[x,y] == pix_val]
+    # if len(growing_neighbours) != 0:
+    #     return growing_neighbours[np.random.choice(len(growing_neighbours))]
+    return legitimate_neighbours[np.random.choice(len(legitimate_neighbours))]
 
 
 def generate():
     pl.figure(1)
     rows, cols = (256, 256)
-    random = np.random.rand()
+    c = mpl.colors.ListedColormap(['white', 'green'])
+    n = mpl.colors.Normalize(vmin=0, vmax=1)
+    # random = np.random.rand()
     # img = np.random.choice([0, 1], size=(rows, cols), p=[random, 1 - random])
-    img1 = np.zeros(shape=(rows, cols//2))
-    img2 = np.ones(shape=(rows, cols//2))
-    img = np.hstack((img1,img2))
-    print(img.shape)
+    # img1 = np.zeros(shape=(rows, cols//2))
+    # img2 = np.ones(shape=(rows, cols//2))
+    # img = img + np.hstack((img1,img2))
+    # print(img.shape)
+    img = np.zeros(shape=(rows, cols))
+    # make random shape inside it
+    # shapes of different sizes
+    circles = []
+    for i in range(50):
+        circles.append((1, 1, np.random.randint(rows), np.random.randint(cols), np.random.randint(20)))
+    for y in range(rows):
+        for x in range(cols):
+            # check if it lies within any one of our circles
+            for (a,b,h,k,r) in circles:
+                if (x/a-h)**2 + (y/b-k)**2 < r**2:
+                    # print('making it 1')
+                    img[y,x] = 1
+    # start sequence generation
     for frame in range(0, 300): # outer loop generates new frames
         start_t = time.time()
         # inner loops generate growth pattern based on
@@ -38,13 +55,13 @@ def generate():
         for i in range(rows):
             for j in range(cols):
                 if img[i,j] == 1:
-                    grow_this_one = get_neighbour(img=img, pixel=(i,j), shape=(rows, cols))
+                    grow_this_one = get_neighbour(img=img, pixel=(i,j), pix_val=0)
                     img[grow_this_one] = 1
         t_elapsed = time.time()-start_t
         pl.clf()
-        pl.imshow(img)
+        pl.imshow(img, cmap=c, norm=n)
         pl.title('Number {} (time elapsed for generation = {:.2f} seconds)'.format(frame, t_elapsed))
-        pl.pause(0.05)
+        pl.pause(0.01)
 
 
 if __name__ == '__main__':
--- PycharmProjects/ForestCoverChange/pipeline/detect.py
+++ PycharmProjects/ForestCoverChange/pipeline/detect.py
@@ -43,20 +43,23 @@ def do(args):
         # saves the image as pkl temporarily
         png_to_pickle(image_file=full_path, pkl_file='tmp.pkl', bands=args.bands)
         # this function returns shape of our used image and raw forest percentage
-        (H, W, C), forest_percentage = batch_wise_inference(model=model, image_path='tmp.pkl',
+        (H, W, C), forest_percentage = batch_wise_inference(model=model, data_type=args.data_type,
+                                                            image_path='tmp.pkl',
                                                             batch_size=20, device=args.device,
                                                             number='tmp', count=count,
                                                             total=len(images_list))
         # and this returns a refined value of forestation
         filtered_forest_percentage = overlay_with_grid(image_path='test_image_tmp.npy',
                                                        pred_path='image_pred_tmp.npy',
+                                                       data_type=args.data_type,
                                                        image_save_path=os.path.join(image_save,
                                                                                     '{}.png'.format(count)),
                                                        downsampled_image_save_path=os.path.join(down_image_save,
                                                                                     '{}.png'.format(count)),
                                                        label_save_path=os.path.join(label_save,
                                                                                     '{}.png'.format(count)),
-                                                       shape=(H,W,C if C == 3 else 3))
+                                                       shape=(H,W,C if C == 3 else 3),
+                                                       type=args.data_type)
         print('\nINFO: {}% Forestation Found.'.format(filtered_forest_percentage))
         forestation.append(filtered_forest_percentage) # forest_percentage
     print(image_save, os.path.join(image_save, 'out.avi'))
@@ -92,6 +95,7 @@ def do(args):
 def main():
     args = ap.ArgumentParser()
     args.add_argument('--images', dest='images_path')
+    args.add_argument('--data', dest='data_type')
     args.add_argument('--model_type', dest='model_name')
     args.add_argument('--channels', type=int, dest='channels')
     args.add_argument('--trained_model', dest='model_path')
