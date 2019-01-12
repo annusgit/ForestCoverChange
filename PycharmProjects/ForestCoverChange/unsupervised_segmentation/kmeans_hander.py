6aa426ca1530871671391bc31ce83c3bb10ff834
AnnusZulfiqar <annusgit@gmail.com>
2019-01-13 02:39:10 +0500

    vector and center prediction added

--- PycharmProjects/ForestCoverChange/unsupervised_segmentation/kmeans_hander.py
+++ PycharmProjects/ForestCoverChange/unsupervised_segmentation/kmeans_hander.py
@@ -4,6 +4,11 @@ from __future__ import print_function, division
 import os
 from k_means import *
 import matplotlib.pyplot as pl
+import torch
+import torch.nn as nn
+from torch.optim import *
+import torch.nn.functional as F
+from model import MODEL
 
 
 def combine_bands(mapped_bands_list, coordinates):
@@ -14,7 +19,7 @@ def combine_bands(mapped_bands_list, coordinates):
     return full_array
 
 
-def main(example_path, clusters_save_path, look_at_images=False):
+def run_clustering(example_path, clusters_save_path, look_at_images=False):
     # first map all bands in memory for reading later on...
     all_bands = []
     for i in range(1,14):
@@ -55,12 +60,161 @@ def main(example_path, clusters_save_path, look_at_images=False):
             count += 1
 
 
+
+##########################################################################################
+############3 these are for analyzing the results
+
+def recombine_clusters_and_view(clusters_save_path):
+    full_test_site_shape = (3663, 5077)
+    # the following variables are for a single slice of the entire image
+    stride = 400
+
+    # row and column wise stride on the entire image
+    full_clustered_image = np.zeros(shape=(stride*int(full_test_site_shape[0]/stride),
+                                           stride*int(full_test_site_shape[1]/stride)))
+    for row in range(0, full_test_site_shape[0]//stride):
+        for col in range(0, full_test_site_shape[1]//stride):
+            this_cluster_save_path = os.path.join(clusters_save_path, '{}_{}_{}_{}.pkl'.format(row*stride,
+                                                                                               (row+1)*stride,
+                                                                                               col*stride,
+                                                                                               (col+1)*stride))
+
+            print("rows {}-{}, columns {}-{}".format(row * stride, (row + 1) * stride,
+                                                     col * stride, (col + 1) * stride))
+            with open(this_cluster_save_path, 'rb') as read_this_one:
+                kmeans_out_subset = pkl.load(read_this_one)
+                array = kmeans_out_subset.labels_.reshape(stride, stride)
+                # print(array.shape)
+                full_clustered_image[row*stride:(row+1)*stride, col*stride:(col+1)*stride] = array
+    pl.imshow(full_clustered_image)
+    pl.show()
+    pass
+
+
+@torch.no_grad()
+def classify_cluster_centers(clusters_save_path, model_path):
+    full_test_site_shape = (3663, 5077)
+    stride = 400
+    # row and column wise stride on the entire image
+    full_classified_image = np.zeros(shape=(stride*int(full_test_site_shape[0]/stride),
+                                            stride*int(full_test_site_shape[1]/stride)))
+
+    # load model for inference
+    model = MODEL(in_channels=13)
+    model.load_state_dict(torch.load(model_path))
+    print('log: loaded saved model {}'.format(model_path))
+    model.eval()
+
+    for row in range(0, full_test_site_shape[0]//stride):
+        for col in range(0, full_test_site_shape[1]//stride):
+            this_cluster_save_path = os.path.join(clusters_save_path, '{}_{}_{}_{}.pkl'.format(row*stride,
+                                                                                               (row+1)*stride,
+                                                                                               col*stride,
+                                                                                               (col+1)*stride))
+            print("rows {}-{}, columns {}-{}".format(row * stride, (row + 1) * stride,
+                                                     col * stride, (col + 1) * stride))
+            with open(this_cluster_save_path, 'rb') as read_this_one:
+                kmeans_subset = pkl.load(read_this_one)
+                kmeans_subset_labels = kmeans_subset.labels_
+                kmeans_subset_cluster_centers = np.expand_dims(kmeans_subset.cluster_centers_, 2)
+                kmeans_subset_cluster_centers_tensor = torch.Tensor(kmeans_subset_cluster_centers)
+                out_x, pred = model(kmeans_subset_cluster_centers_tensor)
+                pred_arr = pred.numpy()
+
+                # now we assign classes to clusters on the bases of their classification results
+                classified_mini_array = kmeans_subset_labels.copy()
+                for u in range(10): # because we have ten clusters in each subset
+                    # u is the cluster number
+                    classified_mini_array[kmeans_subset_labels == u] = pred_arr[u]
+
+                # convert classified array to image
+                mini_image_array = classified_mini_array.reshape(stride, stride)
+                full_classified_image[row*stride:(row+1)*stride, col*stride:(col+1)*stride] = mini_image_array
+    pl.imshow(full_classified_image)
+    pl.show()
+    pass
+
+
+@torch.no_grad()
+def classify_cluster_arrays(example_path, clusters_save_path, model_path):
+    # we'll need the data for it too
+    all_bands = []
+    for i in range(1, 14):
+        all_bands.append(np.load(os.path.join(example_path, '{}.npy'.format(i)), mmap_mode='r'))
+
+    full_test_site_shape = (3663, 5077)
+    stride = 400
+    # row and column wise stride on the entire image
+    full_classified_image = np.zeros(shape=(stride*int(full_test_site_shape[0]/stride),
+                                            stride*int(full_test_site_shape[1]/stride)))
+
+    # load model for inference
+    model = MODEL(in_channels=13)
+    model.load_state_dict(torch.load(model_path))
+    print('log: loaded saved model {}'.format(model_path))
+    model.eval()
+
+    for row in range(0, full_test_site_shape[0]//stride):
+        for col in range(0, full_test_site_shape[1]//stride):
+            this_cluster_save_path = os.path.join(clusters_save_path, '{}_{}_{}_{}.pkl'.format(row*stride,
+                                                                                               (row+1)*stride,
+                                                                                               col*stride,
+                                                                                               (col+1)*stride))
+            print("rows {}-{}, columns {}-{}".format(row * stride, (row + 1) * stride,
+                                                     col * stride, (col + 1) * stride))
+            with open(this_cluster_save_path, 'rb') as read_this_one:
+                kmeans_subset = pkl.load(read_this_one)
+                kmeans_subset_labels = kmeans_subset.labels_
+
+                # predict classes after receiving the data
+                full_array = combine_bands(mapped_bands_list=all_bands, coordinates=(row * stride, (row + 1) * stride,
+                                                                                     col * stride, (col + 1) * stride))
+                samples_vec = full_array.reshape((-1, 13))
+                samples_vec = np.nan_to_num(samples_vec)  # important nan save
+
+                # pick 100 vectors for each cluster, classify and assign to clusters
+                classified_mini_array = kmeans_subset_labels.copy()
+                for u in range(10): # because we have ten clusters in each subset
+                    test_vectors = np.expand_dims(samples_vec[kmeans_subset_labels==u][:100], axis=2)
+                    test_tensor = torch.Tensor(test_vectors)
+                    # print(test_tensor.shape)
+                    out_x, pred = model(test_tensor)
+                    pred_numpy = pred.numpy()
+                    vals, counts = np.unique(pred_numpy, return_counts=True)
+                    this_index = np.argmax(counts)
+                    classified_mini_array[classified_mini_array == u] = vals[this_index]
+                    # print('{}%'.format(counts[this_index]))
+                    # print()
+
+
+                # # now we assign classes to clusters on the bases of their classification results
+                # classified_mini_array = kmeans_subset_labels.copy()
+                # for u in range(10): # because we have ten clusters in each subset
+                #     # u is the cluster number
+                #     classified_mini_array[kmeans_subset_labels == u] = pred_arr[u]
+
+                # convert classified array to image
+                mini_image_array = classified_mini_array.reshape(stride, stride)
+                full_classified_image[row*stride:(row+1)*stride, col*stride:(col+1)*stride] = mini_image_array
+    pl.imshow(full_classified_image)
+    pl.show()
+    pass
+
+
 if __name__ == '__main__':
-    main(example_path='/home/annus/PycharmProjects/ForestCoverChange_inputs_and_numerical_results/'
-                      'full-test-site-pakistan/numpy_sums/2015',
-         clusters_save_path='/home/annus/Desktop/all_clustering_results/2015')
+    # run_clustering(example_path='/home/annus/PycharmProjects/ForestCoverChange_inputs_and_numerical_results/'
+    #                   'full-test-site-pakistan/numpy_sums/2015',
+    #      clusters_save_path='/home/annus/Desktop/all_clustering_results/2015')
 
+    # recombine_clusters_and_view('/home/annus/Desktop/all_clustering_results/2015')
 
+    # classify_cluster_centers(clusters_save_path='/home/annus/Desktop/all_clustering_results/2015',
+    #                               model_path='/home/annus/Desktop/trained_models/model-25.pt')
+
+    classify_cluster_arrays(example_path='/home/annus/PycharmProjects/ForestCoverChange_inputs_and_numerical_results/'
+                                         'full-test-site-pakistan/numpy_sums/2015',
+                            clusters_save_path='/home/annus/Desktop/all_clustering_results/2015',
+                            model_path='/home/annus/Desktop/trained_models/model-25.pt')
 
 
 ############################################################################################################
